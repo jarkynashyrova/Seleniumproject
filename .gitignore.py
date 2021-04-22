94130453ef93060a88cc2642212ff643d585a3d4
jarkynashyrova <83022590+jarkynashyrova@users.noreply.github.com>
2021-04-22 17:58:32 -0400

    init this project

--- .gitignore.py
+++
@@ -1,7 +0,0 @@
-
-
-
-.idea/
-__pycache__/
-.DS_Store ---> option for mac users
-
---
+++ google_search.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome() # or webdriver.Chrome('/path/to/chromedriver')
+
+driver.get("https://google.com")
+
+search_text_box = driver.find_element(By.NAME, "q")
+search_text_box.clear()
+search_text_box.send_keys("python selenium integration")
+search_text_box.send_keys(Keys.RETURN)
+
+
+
